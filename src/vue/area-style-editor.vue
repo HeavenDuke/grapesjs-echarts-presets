4f15edd477b45ffbec4d0e008a16fbd79ebdb322
HeavenDuke <trashlhc@163.com>
2022-07-19 15:04:52 +0800

    补回来了area-style-editor

---
+++ src/vue/area-style-editor.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="gjs-trt-trait">
+        <div class="gjs-label-wrp" data-label>
+            <div class="gjs-label" title="show">{{title || t('grapesjs-echarts-presets.config.areaStyle.label')}}</div>
+        </div>
+        <div class="gjs-field-wrp gjs-field-wrp--text" data-input="">
+            <label class="gjs-field gjs-field-text" data-input="">
+                <button class="btn btn-full" @click="showDialog">{{t('grapesjs-echarts-presets.actions.edit')}}</button>
+            </label>
+        </div>
+        <content-dialog v-if="dialog.visibility" width="450px" :dialog-visibility="dialog.visibility"
+                        :title="dialog.title"
+                        @close="dialog.visibility = false">
+            <div class="gjs-trt-traits gjs-one-bg gjs-two-color">
+                <ep-color-picker :label="t('grapesjs-echarts-presets.config.areaStyle.color.label')"
+                                 v-model="value.color"></ep-color-picker>
+                <ep-number-input :label="t('grapesjs-echarts-presets.config.areaStyle.shadowBlur.label')"
+                                 v-model="value.shadowBlur"></ep-number-input>
+                <ep-color-picker :label="t('grapesjs-echarts-presets.config.areaStyle.shadowColor.label')"
+                                 v-model="value.shadowColor"></ep-color-picker>
+                <ep-number-input :label="t('grapesjs-echarts-presets.config.areaStyle.shadowOffsetX.label')"
+                                 v-model="value.shadowOffsetX"></ep-number-input>
+                <ep-number-input :label="t('grapesjs-echarts-presets.config.areaStyle.shadowOffsetY.label')"
+                                 v-model="value.shadowOffsetY"></ep-number-input>
+                <ep-number-input :label="t('grapesjs-echarts-presets.config.areaStyle.opacity.label')"
+                                 v-model="value.opacity" :max="1" :min="0" :step="0.1"></ep-number-input>
+            </div>
+        </content-dialog>
+    </div>
+</template>
+
+<script>
+
+  import ContentDialog from "./widgets/content-dialog"
+  import EpSelect from "./editor-components/select"
+  import EpOption from "./editor-components/option"
+  import EpInput from "./editor-components/input"
+  import EpCheckBox from "./editor-components/checkbox"
+  import EpColorPicker from "./editor-components/color-picker"
+  import EpNumberInput from "./editor-components/number-input"
+  import EpPositionSelector from "./widgets/position-selector"
+
+  export default {
+    name: "area-style-editor",
+    props: {
+      value: Object,
+      t: Function,
+      title: String
+    },
+    components: {
+      ContentDialog,
+      EpCheckBox,
+      EpSelect,
+      EpOption,
+      EpInput,
+      EpColorPicker,
+      EpNumberInput,
+      EpPositionSelector
+    },
+    watch: {
+      value: {
+        handler(newValue) {
+          this.$emit('input', newValue)
+        },
+        deep: true
+      }
+    },
+    data() {
+      return {
+        dialog: {
+          visibility: false,
+          title: this.t('grapesjs-echarts-presets.dialogs.areaStyle')
+        }
+      }
+    },
+    methods: {
+      showDialog() {
+        this.dialog.visibility = true
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
