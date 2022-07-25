f7b39b3c44b6952efb4681de73d9feff6d9a7dc5
two-Bob <wmbbll@163.com>
2022-07-25 14:37:57 +0800

    grid部分配置完成

--- src/options/title.js
+++ src/options/title.js
@@ -1,5 +1,82 @@
+import {textStyle} from "@/options/shared";
 
-import { textStyle } from "@/options/shared";
+function title(t) {
+  return [{
+    name: "show",
+    type: "Boolean",
+    label: t("grapesjs-echarts-presets.config.title.show.label"),
+    default: true
+  }, {
+    name: "text",
+    type: "String",
+    label: t("grapesjs-echarts-presets.config.title.text.label"),
+    placeholder: t("grapesjs-echarts-presets.config.title.text.placeholder"),
+    default: ""
+  }, {
+    name: "link",
+    type: "String",
+    label: t("grapesjs-echarts-presets.config.title.link.label"),
+    placeholder: t("grapesjs-echarts-presets.config.title.link.placeholder"),
+    default: ""
+  }, {
+      name:'target',
+      type:'Enum',
+      label:t("grapesjs-echarts-presets.config.title.link.label"),
+      placeholder:t("grapesjs-echarts-presets.config.title.link.placeholder"),
+      default:''
+    },{
+    name: "textStyle",
+    type: 'Object',
+    label: t("grapesjs-echarts-presets.config.textStyle.label"),
+    children: textStyle(t)
+  }, {
+    name: "subtext",
+    type: "String",
+    label: t("grapesjs-echarts-presets.config.title.subtext.label"),
+    placeholder: t("grapesjs-echarts-presets.config.title.subtext.placeholder"),
+    default: ""
+  }, {
+    name: "sublink",
+    type: "String",
+    label: t("grapesjs-echarts-presets.config.title.sublink.label"),
+    placeholder: t("grapesjs-echarts-presets.config.title.sublink.placeholder"),
+    default: ""
+  }, {
+    name: "subtextStyle",
+    type: 'Object',
+    label: t("grapesjs-echarts-presets.config.subtextStyle.label"),
+    children: textStyle(t)
+  }, {
+    name: "textAlign",
+    type: "Enum",
+    label: t("grapesjs-echarts-presets.config.title.textAlign.label"),
+    placeholder: t("grapesjs-echarts-presets.config.title.textAlign.placeholder"),
+    default: "auto",
+    candidate:['auto','left','right','center']
+  }, {
+    name: "textVerticalAlign",
+    type: "Enum",
+    label: t("grapesjs-echarts-presets.config.title.textVerticalAlign.label"),
+    placeholder: t("grapesjs-echarts-presets.config.title.textVerticalAlign.placeholder"),
+    default: "auto",
+    candidate:['auto','top','bottom','middle']
+  }, {
+    name: "triggerEvent",
+    type: "Boolean",
+    label: t("grapesjs-echarts-presets.config.title.triggerEvent.label"),
+    default: false,
+  }, {
+    name: "itemGap",
+    type: "Boolean",
+    label: t("grapesjs-echarts-presets.config.title.triggerEvent.label"),
+    default: false,
+  }, {
+    name: "padding",
+    type: "Boolean",
+    label: t("grapesjs-echarts-presets.config.title.triggerEvent.label"),
+    default: false,
+  }];
+}
 
 export default function (t) {
   return {
