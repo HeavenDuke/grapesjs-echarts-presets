aad936d78a3f632f2d74d8ed8d979a931a1defcc
HeavenDuke <trashlhc@163.com>
2022-07-26 19:31:09 +0800

    添加series配置项

--- src/vue/index.vue
+++ src/vue/index.vue
@@ -1,6 +1,16 @@
 <template>
     <chart-section :title="meta.label">
-        <recursion-list :meta="meta.options" v-model="options"/>
+        <recursion-list v-if="!meta.multiple" :meta="meta.options" v-model="options"/>
+        <div class="gjs-traits-group" v-else>
+            <div class="gjs-traits-group-tabs">
+                <div class="gjs-traits-group-tab" v-for="(option, index) in options" @click="switchOption(index)">
+                    {{ option.id || index }}
+                    <small class="gjs-traits-group-tab-close" @click="removeOption(index)">×</small>
+                </div>
+                <div class="gjs-traits-group-tab add" @click="addOption">+</div>
+            </div>
+            <recursion-list :meta="meta.options" v-model="options[optionIndex]"/>
+        </div>
     </chart-section>
 </template>
 
@@ -25,12 +35,31 @@
       }
     },
     data() {
+      let options = constructOptions(this.meta.options)
       return {
-        options: constructOptions(this.meta.options)
+        options: this.meta.multiple ? [options] : options,
+        optionIndex: 0
+      }
+    },
+    methods: {
+      addOption () {
+        this.options = this.options.concat([constructOptions(this.meta.options)])
+      },
+      removeOption(index) {
+        this.options.splice(index, 1)
+        if (this.optionIndex > index) {
+          this.optionIndex--
+        }
+        else if (this.optionIndex === index) {
+          this.optionIndex = Math.min(this.optionIndex, this.options.length - 1)
+        }
+      },
+      switchOption(index) {
+        this.optionIndex = index
       }
     },
     created() {
-      console.log(this.meta, this.options)
+      // console.log(this.meta, this.options)
     }
   };
 </script>
