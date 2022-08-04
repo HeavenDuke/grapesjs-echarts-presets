890d1c5a08785e48fba24b4885963068de511e57
HeavenDuke <trashlhc@163.com>
2022-08-04 19:23:11 +0800

    也许大概是搞定了联动

--- src/options/grid.js
+++ src/options/grid.js
@@ -8,6 +8,14 @@ export default function (t, multiple = false) {
     name: "grid",
     label: t("grapesjs-echarts-presets.dict.group.grid"),
     multiple: false,
+    valid (option) {
+      if (option.series instanceof Array) {
+        return option.series.find(item => item.coordinateSystem === "cartesian2d")
+      }
+      else {
+        return option.series.coordinateSystem === "cartesian2d"
+      }
+    },
     options: [
       {
         name: "show",
--- src/traitsRegister.js
+++ src/traitsRegister.js
@@ -32,7 +32,7 @@ function constructTrait(ec_option) {
     onEvent({component}){
       const { options, meta } = this.inputInstance;
       let overall = JSON.parse(component.get("attributes")["data-ecg-options"] || "{}")
-      overall[meta.name] = options
+      Object.assign(overall[meta.name], options)
       let attributes = {}, name = `data-ecg-${meta.name}`
       attributes[name] = JSON.stringify(options)
       attributes[`${name}-filtered`] = JSON.stringify(extractOptions(overall, meta, options))
